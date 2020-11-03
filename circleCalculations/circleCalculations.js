0a76033a5cae33eaea6f494a0595ea5b70506638
melody <melody@anniecannons.com>
2020-11-03 14:19:13 -0800

    melody-update solution circle

--- circleCalculations/circleCalculations.js
+++ circleCalculations/circleCalculations.js
@@ -4,7 +4,24 @@
 
 //Bonus 2: Make both numbers whole integers (aka round to the nearest integer) 
 
-function circleCalculation () {
-	//your code here
+//   The radius of the circle will be supplied by the user 
 
-circleCalculation(10);
+var radius = parseInt(prompt("What the radius for the circle?"));
+
+function circleCalculation () {
+
+  
+   
+  
+//  program to calculate the area   A=πr2
+  
+   var area = Math.round( Math.PI * radius * Math.pow(radius, 2));
+console.log(area);
+  
+//    calculate the perimeter of a circle C=2πr
+  var circumference = Math.round( 2 * Math.PI * radius);
+  console.log(circumference);
+console.log("The area of the circle is " + area + " and the perimeter of the circle is " + circumference +".");  }
+  circleCalculation(radius);
+  
+  
