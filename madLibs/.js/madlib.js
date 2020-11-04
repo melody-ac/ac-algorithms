b915b06f40771f009cbda7389066abf09f875d7f
melody <melody@anniecannons.com>
2020-11-03 20:09:11 -0800

    melody-madlib solved the exercise.

---
+++ madLibs/.js/madlib.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  
+    function makeMadLib(e) {
+      e.preventDefault();
+      var inputnoun = $( "#noun" ).val();
+      var inputadjective = $( "#adjective" ).val();
+      var inputperson = $("#person").val();
+      var madLibObj = {
+        noun: inputnoun,
+        adjective: inputadjective,
+        person: inputperson
+      }
+   
+    $("#story").append(inputperson + " really loves " + inputadjective + " " + inputnoun +".");
+       }
+    $("#lib-button").click(function(e){
+      makeMadLib(e);
+    });
+    
+    
+  
+    
+  });
